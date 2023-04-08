51457fa4761614807a9e065154962dfd4b04d90d
17ishan <114206488+17ishan@users.noreply.github.com>
2023-04-08 22:51:17 +0530

    Create Cond._statement.cpp

---
+++ Cond._statement.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>=1){
+        cout<<"One";
+    }
+    else if(n>=2){
+        cout<<"Two";
+    }
+    else if(n>=3){
+        cout<<"Three";
+    }
+    else if(n>=4){
+        cout<<"Four";
+    }
+    
+    
+    return 0;
+}
